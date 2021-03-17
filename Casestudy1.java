62b75217e9379a65208f9411711b6942994c89d3
AlvieAsuncion <79246987+AlvieAsuncion@users.noreply.github.com>
2021-03-17 11:54:16 +0800

    Update Casestudy1.java

--- Casestudy1.java
+++ Casestudy1.java
@@ -1,1 +1,24 @@
-Our Casestudy
+
+package casestudy;
+
+
+public class Casestudy1 {
+
+    //Alvie Asuncion
+    static void method1(){
+            System.out.println("My name is Alvie G. Asuncion");
+                    }
+    static void method2(){
+    System.out.println("I'm From Lapaz, tarlac");
+    }
+    static void method3(){
+    System.out.println("I'm 21 yrs old");
+    System.out.println("----------------------------------");
+    }
+    public static void main(String[] args) {
+        method1();
+        method2();
+        method3();
+    }
+    
+}
